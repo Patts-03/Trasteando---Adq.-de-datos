b52fc438ac9737b2833b121775a799610da43193
Paty <202103422@alu.comillas.edu>
2022-09-03 15:54:25 +0200

    Casting results

--- archivo_1.py
+++ archivo_1.py
@@ -3,7 +3,9 @@
 if __name__=="__main__":
     number = input("Try to match our chosen number: ")
 
-    while number != 15:
+    while int(number) != 15:
         print("Ups, that's not our number")
         number = input("Try again : ")
 
+    print("\n Wow, you made it! \n")
+    exit(0)
