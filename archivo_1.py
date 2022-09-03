e246439aa616097d0e5df622070c539ce60e9512
Paty <202103422@alu.comillas.edu>
2022-09-03 15:14:35 +0200

    First change in main

---
+++ archivo_1.py
@@ -0,0 +1,9 @@
+# Voy a trastear un poco
+
+if __name__=="__main__":
+    number = input("Try to match our chosen number: ")
+
+    while number != 15:
+        print("Ups, that's not our number")
+        number = input("Try again : ")
+
